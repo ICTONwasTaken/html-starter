c92dd9ea453097d5bf5fcb0aaab0fe4faa31560b
ICTONwasTaken <158996141+ICTONwasTaken@users.noreply.github.com>
2026-05-17 18:51:23 +0800

    finally fixed this!

--- hugertest.js
+++ hugertest.js
@@ -4,10 +4,10 @@ document.addEventListener("DOMContentLoaded", function() {
   div1.hidden = false;
 
   div1.style.animation = "mymove 0.9s forwards";
-  div1.addEventListener("animationend", myEndFunction); 
-})
-    
-function myEndFunction() { 
+  div1.addEventListener("animationend", myEndFunction, { once: true });
+  
+  function myEndFunction() { 
     this.style.animation = "disappear 0.3s forwards"; 
     div1.hidden = true;
 }
+})
