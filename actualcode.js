ffa637c8c6854915bdd441f74acf6c06b9e6bdbc
ICTONwasTaken <158996141+ICTONwasTaken@users.noreply.github.com>
2026-05-17 19:20:31 +0800

    come

--- actualcode.js
+++ actualcode.js
@@ -30,7 +30,9 @@ window.onload = async () => {
   onValue(ref(db, "numbers/" + something + "/players"), (snapshot) => {
         const players = snapshot.val() || {};
         console.log("Players in room:", Object.keys(players).length);
+        if (players < 1) {
         playerscome();
+        }
     });
   }
 
