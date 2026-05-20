7b53e964e49f3fdf4fbab5bfdef80dd662ef895c
ICTONwasTaken <158996141+ICTONwasTaken@users.noreply.github.com>
2026-05-20 12:08:18 +0800

    295

--- actualcode.js
+++ actualcode.js
@@ -218,6 +218,6 @@ window.openKillPopup = async function() {
 
 window.closeKillPopup = async function() {
   document.getElementById("kill-popup").style.animation = "popout 1s forwards"
-  await new Promise(resolve => setTimeout(resolve, 300));
+  await new Promise(resolve => setTimeout(resolve, 295));
   document.getElementById("kill-popup").style.display = "none";
 }
