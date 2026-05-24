eafd750004878cb0bd7aff5dc7ee5d151d3d0750
ICTONwasTaken <158996141+ICTONwasTaken@users.noreply.github.com>
2026-05-24 23:24:22 +0800

    awdwd

--- actualcode3.js
+++ actualcode3.js
@@ -129,7 +129,7 @@ window.onload = async () => {
               console.log("Last Killed Key:", lastKilledKey);
               div1.hidden = false;
               div1.innerText = playerName + " just died!";
-              div1.style.animation = "mymove 0.9s forwards";
+              div1.style.animation = "mymove 5s forwards";
               div1.addEventListener("animationend", endAnim, { once: true });
           });
         }
